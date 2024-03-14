96ca8e1a10f1906116d325971049628029474f1d
Amanda Santos <113608404+AmandaSoaress@users.noreply.github.com>
2024-03-14 11:53:22 -0300

    mixins

--- src/mixins.js
+++ src/mixins.js
@@ -1,12 +1,8 @@
-import router from './router'
-import store from './store'
-
-
 export const logoutMixin = {
     methods: {
         efetuarLogout() {
-            store.commit("DESLOGAR_USUARIO");
-            router.push({
+          this.$store.commit("DESLOGAR_USUARIO");
+            this.$router.push({
                 name: "login"
             });
         },
